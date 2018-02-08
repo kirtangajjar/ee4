63220315f0503f606f6eaf15b6af49f47cdca1fb
gitlost <burmartke@gmail.com>
2018-02-08 15:02:51 +0000

    Move phpunit PHP directive tests to behat.
    (cherry picked from commit 9021e95683e88ba76bdd3b63991602e968fb1ae7)

--- tests/test-ee.php
+++ tests/test-ee.php
@@ -2,22 +2,6 @@
 
 class EE_Test extends PHPUnit_Framework_TestCase {
 
-	public function testLaunchProcDisabled() {
-		$err_msg = 'Error: Cannot do \'launch\': The PHP functions `proc_open()` and/or `proc_close()` are disabled';
-
-		$cmd = 'php -ddisable_functions=proc_open php/boot-fs.php --skip-wordpress eval ' . escapeshellarg( 'EE::launch( null );' ) . ' 2>&1';
-		$output = array();
-		exec( $cmd, $output );
-		$output = trim( implode( "\n", $output ) );
-		$this->assertTrue( false !== strpos( $output, $err_msg ) );
-
-		$cmd = 'php -ddisable_functions=proc_close php/boot-fs.php --skip-wordpress eval ' . escapeshellarg( 'EE::launch( null );' ) . ' 2>&1';
-		$output = array();
-		exec( $cmd, $output );
-		$output = trim( implode( "\n", $output ) );
-		$this->assertTrue( false !== strpos( $output, $err_msg ) );
-	}
-
 	public function testGetPHPBinary() {
 		$this->assertSame( EE\Utils\get_php_binary(), EE::get_php_binary() );
 	}
