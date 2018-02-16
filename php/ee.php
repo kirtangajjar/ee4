2a5db22d4fa24beaed88c809bae79f997edf72bc
r <riddhesh.sanghvi@rtcamp.com>
2018-02-16 17:14:30 +0530

    Merge changes from remote

--- php/ee.php
+++ php/ee.php
@@ -13,11 +13,5 @@ if ( file_exists( EE_ROOT . '/vendor/autoload.php' ) ) {
 	define( 'EE_VENDOR_DIR', EE_ROOT . '/vendor' );
 }
 
-// Set common headers, to prevent warnings from plugins
-$_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.0';
-$_SERVER['HTTP_USER_AGENT'] = '';
-$_SERVER['REQUEST_METHOD'] = 'GET';
-$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
-
 require_once EE_ROOT . '/php/bootstrap.php';
 \EE\bootstrap();
